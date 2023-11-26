c02a8d3006c6f60d4c1a5aa54f113877b45b0385
OrfeasANikolaou <orfeasnikolaou.dev@gmail.com>
2023-11-26 04:07:46 +0200

    made most of the layout for the program, commiting before small bugfix in case of breaking changes

--- sources/main.cpp
+++ sources/main.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
 #include <ncurses/curses.h>
+#include "../headers//menu.hpp"
 #include "../headers/ncurses_helpers.hpp" // ncurses already included
+enum mfile{new_session, statistics, quit};
+enum mopts{set_timer};
+enum mhelp{about};
 int main(void){
 	/* START INIT */
 	initscr();
@@ -21,23 +25,41 @@ int main(void){
 	change_background_color(1);
 	/* END INIT */
 
-	WINDOW* title_win = newwin(40, 150, LINES/2-20, COLS/2-75);
-	WINDOW* menu_win = newwin(38,148, LINES/2-19, COLS/2-74);
+		WINDOW* title_win = newwin(40, 150, LINES/2-20, COLS/2-75);
+	WINDOW* menu_bar_win = newwin(38,148, LINES/2-19, COLS/2-74);
 	box(title_win, 0, 0);
-	box(menu_win, 0, 0);
+	box(menu_bar_win, 0, 0);
 	mvwprintw(title_win,0,69,"POMODORO_TUI");
 	move(0,0);
 	
 	wrefresh(title_win);
-	wrefresh(menu_win);
+	wrefresh(menu_bar_win);
 	refresh();
 
 	leaveok(stdscr, 1);
-	leaveok(menu_win, 1);
+	leaveok(menu_bar_win, 1);
 	leaveok(title_win, 1);
 	
+	std::string menu_file[] = {"NEW SESSION", "STATISTICS", "QUIT", };
+	std::string menu_opts[] = {"SET TIMER", };
+	std::string menu_help[] = {"ABOUT", };
+
+	Menu menus[3] = {
+										Menu("FILE", 'f', menu_file, sizeof(menu_file) / sizeof(std::string)),
+										Menu("OPTS", 'o', menu_opts, sizeof(menu_opts) / sizeof(std::string)),
+										Menu("HELP", 'h', menu_help, sizeof(menu_help) / sizeof(std::string)),
+									};
+
+	MenuBar menu_bar(menu_bar_win, menus, sizeof(menus) / sizeof(Menu));
+	menu_bar.draw();
+	int ch;
+	while ( (ch = wgetch(menu_bar_win)) ){
+		menu_bar.handle_trigger(ch);
+		menu_bar.draw();
+	}
+
 	delwin(title_win);
-	delwin(menu_win);
+	delwin(menu_bar_win);
 
 	getch();
 	endwin();
--- sources/menu.cpp
+++ sources/menu.cpp
@@ -1,3 +1,96 @@
 #include "../headers/menu.hpp"
+#include <ncurses/curses.h>
+
+Menu::Menu(std::string txt, int trgr, std::string* itms, int n_items) :
+	text(txt), trigger(trgr), items(itms), num_items(n_items), selected_item(0) {}
+
+void Menu::select_next_item(void){
+	++this->selected_item;
+	if (this->selected_item >= this->num_items) this->selected_item = 0;
+}
+void Menu::select_previous_item(void){
+	--this->selected_item;
+	if (this->selected_item < 0) this->selected_item = num_items - 1;
+}
 
 
+MenuBar::MenuBar(WINDOW* w, Menu* m, int n) : 
+	win(w), menus(m), num_menus(n), selected_menu(-1){
+		int curr_pos = 2;
+		for (int i = 0; i < this->num_menus; ++i){
+			this->menus[i].start_x_pos = curr_pos;
+			curr_pos += this->menus[i].text.length() + 1;
+
+		}
+		int yMax, xMax, yBeg, xBeg;
+		getmaxyx(this->win, yMax, xMax);
+		getbegyx(this->win, yBeg, xBeg);
+		this->menu_win = newwin(yMax - 2, xMax - 2, yBeg + 1, xBeg + 1);
+		keypad(this->menu_win, true);
+		wrefresh(this->menu_win);
+}
+
+void MenuBar::draw(void){
+
+	for (int i = 0; i < this->num_menus; ++i){
+		this->draw_menu(this->menus[i], this->selected_menu == i);
+	}
+	this->selected_menu = -1;
+}
+
+void MenuBar::handle_trigger(int trigger){
+	for (int i = 0; i < this->num_menus; ++i){
+		if (trigger == this->menus[i].trigger ||
+				trigger - 32 == this->menus[i].trigger){ /* ascii black magic so it works with capital */ 
+	 		this->selected_menu = i;							 /*	letters too */
+		}
+	}
+}
+
+void MenuBar::reset(void){
+	for (int i = 0; i < this->num_menus; ++i){
+		int temp_x = this->menus[i].start_x_pos;
+		std::string temp_txt = this->menus[i].text;
+		mvwprintw(this->win, 0, temp_x, "%s", temp_txt.c_str());
+	}
+	wrefresh(this->win);
+}
+
+void MenuBar::draw_menu(Menu menu, bool is_selected){
+	int start_x = menu.start_x_pos;
+	std::string text = menu.text;
+	if (is_selected) { wattron(this->win, A_STANDOUT); }
+	mvwprintw(this->win, 0, start_x, "%s", text.c_str());
+	wattroff(this->win, A_STANDOUT);
+	wrefresh(this->win);
+	int ch;
+	draw_menu_items(menu);
+	wrefresh(this->menu_win);
+	while (is_selected && 
+				(ch = wgetch(this->menu_win))){
+		switch(ch){
+			case KEY_UP: case 'k': case 'K':
+				menu.select_previous_item();
+				break;
+			case KEY_DOWN: case 'j': case 'J':
+				menu.select_next_item();
+				break;
+			default:
+				is_selected = false;
+				break;
+		}
+		draw_menu_items(menu);	
+	}
+	werase(this->menu_win);
+	wrefresh(this->menu_win);
+	reset();
+}
+
+void MenuBar::draw_menu_items(Menu menu){
+	int xMax = getmaxx(this->menu_win);
+	for (int i = 0; i < menu.num_items; ++i){
+		mvwprintw(this->menu_win, i, 0, "%s", menu.items[i].c_str());
+		if (menu.selected_item == i) { mvwchgat(this->menu_win, i, 0, xMax, A_NORMAL, 1, NULL); }
+		else { mvwchgat(this->menu_win, i, 0, xMax, A_STANDOUT, 1, NULL); }
+	}
+}
